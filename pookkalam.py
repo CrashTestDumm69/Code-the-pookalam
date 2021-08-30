6ee7900e8b1d09d79bd497883ea3247efc61e2d5
CrashTestDumm69 <67448059+CrashTestDumm69@users.noreply.github.com>
2021-08-30 21:24:34 +0530

    Add files via upload

---
+++ pookkalam.py
@@ -0,0 +1,270 @@
+from turtle import *
+
+bgcolor("black")
+pensize(5)
+speed(0)
+
+right(45)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+right(180)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+right(90)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+right(180)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+right(45)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+right(180)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+right(90)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+right(180)
+
+begin_fill()
+color("brown")
+circle(145)
+end_fill()
+
+begin_fill()
+color("orange")
+circle(140)
+end_fill()
+
+begin_fill()
+color("yellow")
+circle(130)
+end_fill()
+
+begin_fill()	
+color("white")
+circle(120)
+end_fill()
+
+color("maroon")
+dot(420)
+color("brown")
+dot(410)
+
+color('#6EFE24')
+for i in range(4):
+    begin_fill()
+    circle(143,90)
+    left(90)
+    circle(143,90)
+    end_fill()
+
+color('#5CD120')
+for i in range(4):
+    begin_fill()
+    circle(113,90)
+    left(90)
+    circle(113,90)
+    end_fill()
+
+color("green")
+for i in range(4):
+    begin_fill()
+    circle(83,90)
+    left(90)
+    circle(83,90)
+    end_fill()
+
+color("darkgreen")
+for i in range(4):
+    begin_fill()
+    circle(53,90)
+    left(90)
+    circle(53,90)
+    end_fill()
+
+for i in range(4):
+    color("darkgreen")
+    circle(143,90)
+    left(90)
+    circle(143,90)
+    
+right(45)
+
+color("purple")
+for i in range(4):
+    begin_fill()
+    circle(143,90)
+    left(90)
+    circle(143,90)
+    end_fill()
+
+color("#E23DFF")
+for i in range(4):
+    begin_fill()
+    circle(113,90)
+    left(90)
+    circle(113,90)
+    end_fill()
+
+color('#FD66FF')
+for i in range(4):
+    begin_fill()
+    circle(83,90)
+    left(90)
+    circle(83,90)
+    end_fill()
+
+color("pink")
+for i in range(4):
+    begin_fill()
+    circle(53,90)
+    left(90)
+    circle(53,90)
+    end_fill()
+
+for i in range(4):
+    color("purple")
+    circle(143,90)
+    left(90)
+    circle(143,90)
+    
+color("brown")
+dot(20)
+
+hideturtle()
+done()
